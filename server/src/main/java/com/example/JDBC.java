9b485a7dd4fc919f20fa86f9b95e9ca5b7d8536e
Neltharion Earth-Warden <loliikonn@mail.ru>
2023-12-03 20:08:34 +0500

    JDBC

--- server/src/main/java/com/example/JDBC.java
+++ server/src/main/java/com/example/JDBC.java
@@ -1,6 +1,7 @@
 package com.example;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.Statement;
 
@@ -13,7 +14,9 @@ public class JDBC {
 
     }
 
-    public static void connect() throws ClassNotFoundException {
+    public static void connect() throws Exception {
         Class.forName("org.sqlite.JDBC");
+        connection = DriverManager.getConnection("jdbc:sqlite:whatsApp.db");
+        stmt = connection.createStatement();
     }
 }
